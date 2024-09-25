d46f356aa9bf02ba4db0c07c57cf30426bc58bc1
hardycathal <hardyfcathal@gmail.com>
2024-09-25 14:51:11 +0100

    Added a method called greetByName that accepts @PathVariable

--- src/main/java/ie/atu/week_2_cathal_hardy/RequestController.java
+++ src/main/java/ie/atu/week_2_cathal_hardy/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.week_2_cathal_hardy;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,11 @@ public class RequestController {
     public String sayHello(){
         return "Hello World!";
     }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name) {
+        return "Howdy, " + name + "!";
+    }
 }
+
+
