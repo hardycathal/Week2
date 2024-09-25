9bf836c7e070d07f9229eb3ef5654dca3776d974
hardycathal <hardyfcathal@gmail.com>
2024-09-25 15:33:53 +0100

    Created a calculator method

--- src/main/java/ie/atu/week_2_cathal_hardy/RequestController.java
+++ src/main/java/ie/atu/week_2_cathal_hardy/RequestController.java
@@ -18,6 +18,29 @@ public class RequestController {
     public String details(@RequestParam String name, @RequestParam int age){
         return "Name: " + name + "\nAge: " + age;
     }
+
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation){
+        int result = 0;
+        switch(operation){
+            case "add":
+                result = num1 + num2;
+                break;
+            case "subtract":
+                result = num1 - num2;
+                break;
+            case "multiply":
+                result = num1 * num2;
+                break;
+            case "divide":
+                if(num2==0){
+                    return "Error, num2 must be greater than 0!";
+                }
+                result = num1 / num2;
+                break;
+        }
+        return "Result: " + result;
+    }
 }
 
 
