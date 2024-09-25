04dbea0835b510fc466d61f7a74cbf99786f4ab8
hardycathal <hardyfcathal@gmail.com>
2024-09-25 14:43:09 +0100

    Added method that returns hello with @getmapping

---
+++ src/main/java/ie/atu/week_2_cathal_hardy/RequestController.java
@@ -0,0 +1,13 @@
+package ie.atu.week_2_cathal_hardy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
