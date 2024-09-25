c3f1cfd316da3dc9edd74e6984d14e6a85493918
hardycathal <hardyfcathal@gmail.com>
2024-09-25 14:29:19 +0100

    Created new projected named Week2

---
+++ src/main/java/ie/atu/week_2_cathal_hardy/Week2CathalHardyApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week_2_cathal_hardy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2CathalHardyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week2CathalHardyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week_2_cathal_hardy/Week2CathalHardyApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week_2_cathal_hardy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2CathalHardyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
